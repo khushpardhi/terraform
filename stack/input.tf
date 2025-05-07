7fa1d6dc5d18d33b21b834c2540ca553be21a6f0
khushpardhi <128688910+khushpardhi@users.noreply.github.com>
2025-05-07 12:25:49 +0530

    Update input.tf

--- stack/input.tf
+++ stack/input.tf
@@ -20,5 +20,9 @@ variable "security_group_name" {
   description = "this is my routtable_name"
   default = "MySecurityGroup"
 }
-
+variable "security_group_name" {
+  type = string
+  description = "this is my routtable_name"
+  default = "MySecurityGroup"
+}
 #variable "ec2_sg_id" {}
